9255d7783ae829404e786fa8153fcab3e409b722
Andrew Paterson <andrew@hyraxbio.co.za>
2019-01-21 15:02:27 +0200

    Fixed bug in collapse menu icon.

--- addon/components/svg-repo/icons/icon-clipboard.js
+++ addon/components/svg-repo/icons/icon-clipboard.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../../../templates/components/svg-repo/icons/icon-clipboard';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: ''
 });
