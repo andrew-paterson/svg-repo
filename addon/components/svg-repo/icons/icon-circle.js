4fd67d8176f63e076fcf0ed99a8ebea611fa4fe5
Andrew Paterson <andrew@hyraxbio.co.za>
2019-01-23 10:33:42 +0200

    Added circle icon.

---
+++ addon/components/svg-repo/icons/icon-circle.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/svg-repo/icons/icon-circle';
+
+export default Component.extend({
+  tagName: '',
+  layout
+});
