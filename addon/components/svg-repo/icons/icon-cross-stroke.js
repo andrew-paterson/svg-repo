0e08d173d6b757c0a5eb4fcc12324200fb2439da
Andrew Paterson <andrew@hyraxbio.co.za>
2019-11-27 15:39:32 +0200

    Added new icons.

---
+++ addon/components/svg-repo/icons/icon-cross-stroke.js
@@ -0,0 +1,6 @@
+import SvgBase from 'svg-repo/components/svg-base';
+import layout from '../../../templates/components/svg-repo/icons/icon-cross-stroke';
+
+export default SvgBase.extend({
+  layout
+});
