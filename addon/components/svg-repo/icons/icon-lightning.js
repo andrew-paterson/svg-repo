bd1cc4c8f64269cdbe69ced79521908cf0d64456
Andrew Paterson <andrew.paterson9@gmail.com>
2021-09-13 15:31:10 +0200

    Fix more merge conflicts

--- addon/components/svg-repo/icons/icon-lightning.js
+++ addon/components/svg-repo/icons/icon-lightning.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-import Component from '@ember/component';
-import layout from '../../../templates/components/svg-repo/icons/icon-lightning';
-
-export default Component.extend({
-  layout,
-  tagName: ''
-});
-=======
 import SvgBase from 'svg-repo/components/svg-base';
 import layout from '../../../templates/components/svg-repo/icons/icon-lightning';
 
 export default SvgBase.extend({
   layout,
 });
->>>>>>> develop
--- addon/components/svg-repo/icons/icon-network-server.js
+++ addon/components/svg-repo/icons/icon-network-server.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import Component from '@ember/component';
-import layout from '../../../templates/components/svg-repo/icons/icon-network-server';
-
-export default Component.extend({
-  layout,
-=======
 import SvgBase from 'svg-repo/components/svg-base';
 import layout from '../../../templates/components/svg-repo/icons/icon-network-server';
 
 export default SvgBase.extend({
   layout
->>>>>>> develop
 });
