04b3f577fcbdda8772627383704a59e634c86b35
Andrew Paterson <andrew.paterson9@gmail.com>
2022-04-12 12:09:16 +0200

    Add switch icon

---
+++ addon/components/svg-repo/icons/icon-switch.js
@@ -0,0 +1,7 @@
+import SvgBase from 'svg-repo/components/svg-base';
+import layout from '../../../templates/components/svg-repo/icons/icon-switch';
+
+export default SvgBase.extend({
+  layout,
+  tagName: ''
+});
