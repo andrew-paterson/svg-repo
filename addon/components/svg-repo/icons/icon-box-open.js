abb739bf2304228e124c7a83f18d8a4d0574ddf7
Andrew Paterson <andrew.paterson9@gmail.com>
2022-10-07 15:00:52 +0200

    Add box-open icon

---
+++ addon/components/svg-repo/icons/icon-box-open.js
@@ -0,0 +1,6 @@
+import SvgBase from 'svg-repo/components/svg-base';
+import layout from '../../../templates/components/svg-repo/icons/icon-box-open';
+
+export default SvgBase.extend({
+  layout
+});
